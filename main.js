c92eb66fccf738e8953a7e7972dbcd6eba8610f0
mswatermelon <aristarkhova_veronika@mail.ru>
2016-08-12 14:41:19 +0300

    Added comments

--- main.js
+++ main.js
@@ -3,11 +3,13 @@
  */
 'use strict';
 
+// "Хелпер" для соединения имени и фамилии
 Handlebars.registerHelper( 'concat', function(f,l) {
     return f + " " + l;
 });
 
 new Promise(function (resolve) {
+    // Если страница загружена перевести промис в состояние resolved
     if(document.readyState == "complete"){
         resolve();
     }
@@ -15,11 +17,14 @@ new Promise(function (resolve) {
         window.onload = resolve;
     }
 }).then(function () {
+    // Затем инициализировать приложение
     return new Promise(function (resolve, reject) {
         VK.init({
            apiId: 5583715
         });
+        // Попробовать залогиниться
         VK.Auth.login(function (response) {
+            // Если пользователь залогинился перевести промис в состояние resolved
             if (response.session){
                 resolve(response);
             }
@@ -30,11 +35,13 @@ new Promise(function (resolve) {
     });
 }).then(function () {
     return new Promise(function (resolve, reject) {
+        // Получить информацию о пользователе, чьих друзей мы будем выводить
         VK.api('users.get', {'name_case': 'gen', 'v': '5.53'}, function (response) {
             if (response.error){
                 reject(new Error(response.error.error_msg));
             }
             else{
+                // Добавить заголовок с именем и фамилией человека, чьих друзей мы будем выводить
                 let headerInfo = document.getElementById('headerInfo'),
                     user = response.response[0];
 
@@ -45,11 +52,13 @@ new Promise(function (resolve) {
     });
 }).then(function () {
     return new Promise(function (resolve, reject) {
+        // Получить список друзей пользователей, включая фото и дату рождения
         VK.api('friends.get', {'fields':'photo_50,bdate', 'v': '5.53'}, function (response) {
             if (response.error){
                 reject(new Error(response.error.error_msg));
             }
             else{
+                // Если запрос успешен получить шаблон и контейнер
                 let mainTemplate = document.getElementById('main-template'),
                     results = document.getElementById('results'),
                     friends = response.response.items,
@@ -59,34 +68,46 @@ new Promise(function (resolve) {
                     friendsWYDate = [],
                     friendsODate = [];
 
+                // Проходим циклом по списку друзей
                 for (let i = 0; i < friends.length; i++){
+                    // Если в дате рождения есть день, месяц, год
                     if (friends[i].bdate && friends[i].bdate.split('.').length == 3) {
+                        // Преобразуем bdate в дату, которую воспримет new Date
                         friends[i].date = friends[i].bdate.split('.').reverse();
-                        friends[i].age = Math.abs(new Date(new Date() - new Date(friends[i].date)).getUTCFullYear() - 1970);
+                        // Посчитаем возраст
+                        friends[i].age = Math.abs(new Date(new Date() - new Date(friends[i].date))
+                                .getUTCFullYear() - 1970);
+                        // Добавим этих друзей в список друзей с датой рождения
                         friendsWDate.push(friends[i]);
                     }
                     else if (friends[i].bdate && friends[i].bdate.split('.').length == 2) {
+                        // Если в дате есть число и месяц добавим в список друзей с датой рождения,
+                        //  в которой не указан год
                         friendsWYDate.push(friends[i]);
                     }
                     else{
+                        // Всех остальных добавим в другой список.
                         friendsODate.push(friends[i]);
                     }
                 }
 
+                // Отсортируем массив друзей по датам рождения
                 friendsWDate.sort(function (a,b) {
                     return new Date(b.date) - new Date(a.date);
                 });
 
+                // Добавим друзей, у которых не указан год рождения в список
                 friendsWDate = friendsWDate.concat(friendsWYDate);
+                // Добавим друзей, у которых не указана дата рождения
                 friendsWDate = friendsWDate.concat(friendsODate);
 
+                // Вставить в соответствие с шаблоном информацию о друзьях
                 results.innerHTML = templateFn({ list: friendsWDate });
 
                 resolve();
             }
         })
     });
-
 }).catch(function (e) {
     alert(`Возникла ошибка: ${e.message}`)
 });
